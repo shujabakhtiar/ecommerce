b68bcfd9910572f2956e7687ecdbf4ec5353e8e4
shujabakhtiar <shujabaktiar@gmail.com>
2020-08-23 02:42:57 +0530

    just authentication completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,84 @@
 import React from "react";
+import { Auth, Hub } from "aws-amplify";
+import { withAuthenticator, AmplifyTheme } from "aws-amplify-react";
 import "./App.css";
-
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import MarketPage from "./pages/MarketPage";
+import { Authenticator } from "aws-amplify-react/dist/Auth";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 class App extends React.Component {
-  state = {};
+  state = {
+    user: null,
+  };
+
+  componentDidMount() {
+    this.getUserData();
+    Hub.listen("auth", this, "onHubCapsule");
+  }
+  getUserData = async () => {
+    const user = await Auth.currentAuthenticatedUser();
+    user ? this.setState({ user }) : this.setState({ user: null });
+  };
+  onHubCapsule = (capsule) => {
+    switch (capsule.payload.event) {
+      case "signIn":
+        console.log("Signned in");
+        this.getUserData();
+        break;
+      case "signUp":
+        console.log("signedUp");
+        break;
+      case "signOut":
+        console.log("signed out");
+        this.setState({ user: null });
+        break;
+      default:
+        return;
+    }
+  };
+  handleSignOut = async () => {
+    try {
+      await Auth.signOut();
+    } catch (err) {
+      console.error("Erron signing out", err);
+    }
+  };
 
   render() {
-    return <div>App</div>;
+    const { user } = this.state;
+
+    return !user ? (
+      <Authenticator theme={theme} />
+    ) : (
+      <Router>
+        <React.Fragment>
+          <Navbar user={user} handleSignOut={this.handleSignOut} />
+          {/*Routes*/}
+          <div className="app-container">
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/profile" component={ProfilePage} />
+            <Route
+              exact
+              path="/markets/:marketId"
+              component={({ match }) => (
+                <MarketPage marketId={match.params.marketId} />
+              )}
+            />
+          </div>
+        </React.Fragment>
+      </Router>
+    );
   }
 }
+const theme = {
+  ...AmplifyTheme,
+  navBar: {
+    ...AmplifyTheme.navBar,
+    backgroundColor: "#808080",
+  },
+};
 
+//export default withAuthenticator(App, true, [], null, theme);
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,39 @@
 import React from "react";
-// import { Menu as Nav, Icon, Button } from "element-react";
-
-const Navbar = () => <div>Navbar</div>;
+import { Menu as Nav, Icon, Button } from "element-react";
+import { NavLink } from "react-router-dom";
+const Navbar = ({ user, handleSignOut }) => (
+  <Nav mode="horizontal" theme="dark" defaultActive="1">
+    <div className="nav-container">
+      <Nav.Item index="1">
+        <NavLink to="/" className="nav-link">
+          <span className="app-title">
+            <img
+              src="http://icon.now.sh/account_balance/f90"
+              alt="App icon"
+              className="app-icon"
+            />
+            Logoo
+          </span>
+        </NavLink>
+      </Nav.Item>
+      <div className="nav-items">
+        <Nav.Item index="2">
+          <span className="app-user">Hello,{user.username}</span>
+        </Nav.Item>
+        <Nav.Item>
+          <NavLink to="/profile" className="nav-link">
+            <Icon name="setting" />
+            Profile
+          </NavLink>
+        </Nav.Item>
+        <Nav.Item index="4">
+          <Button type="warning" onClick={handleSignOut}>
+            Sign Out
+          </Button>
+        </Nav.Item>
+      </div>
+    </div>
+  </Nav>
+);
 
 export default Navbar;
